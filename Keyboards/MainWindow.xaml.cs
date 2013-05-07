f04b03030d9777ab0ec98b7a32c6087ec4ac220d
Lefteris Kororos <lkororos@gmail.com>
2013-05-07 14:05:21 +0300

    Added keyboard initial code drop

---
+++ Keyboards/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CNParser;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Keyboards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //In order for the application to work properly Windows Vista is
+            //the minimum required OS
+            if (CommonFileDialog.IsPlatformSupported != true)
+            {
+                MessageBox.Show("The application requires at least Windows Vista to work correctly.", "OS Not Supported", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+                return;
+            }
+        }
+
+        private void testbutton_Click(object sender, RoutedEventArgs e)
+        {
+            String inFileName = null;
+            String outFilename = null;
+
+            
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".txt.";
+            dlg.Filter = "Text Files|*.txt";
+            dlg.Title = "Select input file";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                inFileName = dlg.FileName;
+            }
+            
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select Output Folder";
+            CommonFileDialogResult dirResult = dialog.ShowDialog();
+
+            if (dirResult == CommonFileDialogResult.Ok)
+            {
+                outFilename = dialog.FileName + "\\outfile.txt";
+            }
+            
+            //MessageBox.Show("inFile:\t" + inFileName + "\nOutFile:\t" + outFilename);
+            if ((inFileName != null) && (outFilename != null))
+            {
+                LKParser parser = new LKParser(inFileName, outFilename);
+                MessageBox.Show("inFile:\t" + parser.getInPath() + "\nOutFile:\t" + parser.getOutPath());
+
+                parser.readFileAsync();
+            }
+        }
+    }
+}
---
+++ LKParser/LKParser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNParser
+{
+    public class LKParser
+    {
+        //public FileStream inFile { get; set; }
+        //public FileStream outFile { get; set; }
+
+        private String inFileName;
+        private String outFileName;
+        private String inPath;
+        private String outPath;
+        private char[] inBuffer;
+
+        private Dictionary<String, Int16> lettersDic;
+        private Dictionary<String, Int16> combosDic;
+
+        public LKParser(String inputFile, String outputFile)
+        {
+            inFileName = inputFile;
+            outFileName = outputFile;
+            lettersDic = new Dictionary<String, Int16>(StringComparer.OrdinalIgnoreCase);
+            combosDic = new Dictionary<String, Int16>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public String getInPath()
+        {
+            return inPath;
+        }
+
+        //TODO: Currently not used
+        private String getPath(FileStream fileStream)
+        {
+            return fileStream.Name;
+        }
+
+        public String getOutPath()
+        {
+            return outPath;
+        }
+
+        public async void readFileAsync()
+        {
+            FileStream inFS = null;
+            try
+            {
+                inFS = new FileStream(inFileName, FileMode.Open, FileAccess.Read);
+                using(StreamReader txtReader = new StreamReader(inFS))
+                {
+                    inBuffer = new char[txtReader.BaseStream.Length];
+                    await txtReader.ReadAsync(inBuffer,0,(int)inBuffer.Length);
+                }
+                foreach(char c in inBuffer)
+                {
+                    if (lettersDic.ContainsKey(c.ToString()))
+                        lettersDic[c.ToString()]++;
+                    else
+                        lettersDic[c.ToString()] = 1;
+                }
+            }
+            finally
+            {
+                if (inFS != null)
+                    inFS.Dispose();
+            }
+            //using(new FileStream(inFileName, FileMode.Open, FileAccess.Read),
+              //                                 new FileStream(outFilename, FileMode.Create, FileAccess.Write)
+        }
+    }
+}
